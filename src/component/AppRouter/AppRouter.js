a86f4bcce125812a5fb8c4f8565f9ea5399d75bd
rintumr <rmrintu@gmail.com>
2019-03-22 15:57:36 +0530

    Fixed modals

--- src/component/AppRouter/AppRouter.js
+++ src/component/AppRouter/AppRouter.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
 
 import './AppRouter.css';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Modal from '../Modal/Modal';
 
 class AppRouter extends Component {
 
-    constructor() {
+    constructor(props) {
+        console.log("AppRouter", props);
         super();
         this.state = {
             message: '',
@@ -31,54 +27,10 @@ class AppRouter extends Component {
         this.toggleModal();
     }
 
-    // Function to toggle Modal
-
-    toggleModal = () => {
-        let data = { ...this.state };
-        data.showModal = !data.showModal;
-        this.updateState(data);
-    }
-
-    updateState(value) {
-        this.setState(value);
-    }
-
     render() {
-        let display = null;
-        console.log(this.state.showModal);
-        if (this.state.showModal) {
-            display = <Modal data={this.state} onClose={this.toggleModal} />
-        }
-
         return (
             <div>
-                {display}
-                <Router>
-                    <div>
-                        <h1 className="header">
-                            Welcome to <span className="title">CV Hunt</span>
-                        </h1>
-                        <hr />
-                        <this.Header />
-                        <Route path="/login" component={() => <Login changeState={this.stateHandler} />} />
-                        <Route path="/register" render={() => <Register changeState={this.stateHandler} />} />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
-
-    // Function to display the nav button
-
-    Header() {
-        return (
-            <div>
-                <Link to="/login">
-                    <Button variant="success">Sign In</Button>
-                </Link>
-                <Link to="/register">
-                    <Button variant="danger">New User</Button>
-                </Link>
+                <p>Welcome to CV HUNT</p>
             </div>
         );
     }
--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
@@ -6,61 +6,92 @@ import Register from '../component/Register/Register';
 import Login from '../component/Login/Login';
 import AppRouter from '../component/AppRouter/AppRouter';
 import JobListing from '../component/JobListing/JobListing';
+import Modal from '../component/Modal/Modal';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      status: '',
+      success: '',
+      showModal: false
+    }
+  }
+
   render() {
-
+    let display = null;
+    console.log(this.state.showModal);
+    if (this.state.showModal) {
+      display = <Modal data={this.state} onClose={this.toggleModal} />
+    }
     return (
-      <div className="navbar">
-        <Router>
-          <div className="col-md-12 row">
-            <div className="col-md-6 nav navbar-nav navbar-left">
-              <p>CV Hunt</p>
+      <Fragment>
+        <div>
+          {display}
+        </div>
+        <div className="navbar">
+          <Router>
+            <div className="col-md-12 row">
+              <div className="col-md-6 nav navbar-nav navbar-left">
+                <p>CV Hunt</p>
+              </div>
+              <div className=" col-md-6 nav navbar-nav navbar-right">
+                <ul>
+                  <li>
+                    <Link to="/login">
+                      Sign In
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/register">
+                      New User
+                     </Link>
+                  </li>
+                  <li>
+                    <Link to="/jobs">
+                      Job Listing
+                     </Link>
+                  </li>
+                </ul>
+
+              </div>
+              <Route exact path="/" component={AppRouter} />
+              <Route exact path="/login" render={() => <Login changeState={this.stateHandler} />} />
+              <Route exact path="/register" render={() => <Register changeState={this.stateHandler} />} />
+              <Route exact path="/jobs" component={JobListing} />
             </div>
-            <this.Header />
-            <Route path="/" component={AppRouter} />
-            <Route path="/login" render={() => <Login changeState={this.stateHandler} />} />
-            <Route path="/register" render={() => <Register changeState={this.stateHandler} />} />
-            <Route path="/jobs" component={JobListing}/>
-          </div>
-        </Router>
-      </div>
+          </Router>
+        </div>
+      </Fragment>
     );
 
   }
 
-  stateHandler(modalToggle){
-    console.log("stateHandler",modalToggle);
-    let modal = null
-    modal=<AppRouter data={modalToggle}/>
-    console.log("stateHandler123",modal);
-    return modal;
+  // Function to handle the state
+
+  stateHandler = (value) => {
+    let data = { ...this.state };
+    data.message = value.message;
+    data.status = value.status;
+    data.success = value.success;
+    this.updateState(data);
+    this.toggleModal();
   }
 
-  Header() {
-    return (
-      <div className=" col-md-6 nav navbar-nav navbar-right">
-        <ul>
-          <li>
-            <Link to="/login">
-              Sign In
-            </Link>
-          </li>
-          <li>
-            <Link to="/register">
-              New User
-            </Link>
-          </li>
-          <li>
-            <Link to="/jobs">
-               Job Listing
-            </Link>
-          </li>
-        </ul>
+  // Function to toggle modal
 
-      </div>
-    );
+  toggleModal = () => {
+    let data = { ...this.state };
+    data.showModal = !data.showModal;
+    this.updateState(data);
+  }
+
+  // Function to update state
+
+  updateState = (value) => {
+    this.setState(value);
   }
 
 }
